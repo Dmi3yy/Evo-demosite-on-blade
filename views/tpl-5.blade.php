0e74d1d2d78dbf7f9d2768f7fa9aaa378c5d628d
Dmi3yy <dmi3yy@gmail.com>
2018-12-17 10:55:37 +0300

    second

--- views/tpl-5.blade.php
+++ views/tpl-5.blade.php
@@ -1,1 +1,21 @@
-@extends('layouts.app')
+1234
+<?php
+dump($documentObject)
+?>
+{{ $modx->documentObject['image'][1] }}
+<hr />
+@evoParser($documentObject['titl'])
+<hr />
+@evoParser('[*image*] <br /> [*pagetitle*]')
+<hr />
+@evoLang('save')
+<hr />
+@evoStyle('favicon')
+<hr />
+@evoAdminLang
+<hr />
+@evoCharset
+<hr />
+@evoAdminThemeUrl
+<hr />
+@evoAdminThemeName
