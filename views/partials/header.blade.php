b45dc7ad7a3545bb9524dae96d790947d76bad38
Dmi3yy <dmi3yy@gmail.com>
2019-08-19 00:05:29 +0300

    topmenu - done

--- views/partials/header.blade.php
+++ views/partials/header.blade.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-sm-12" itemscope itemtype="http://schema.org/Organization">
 
-				<a class="logo" href="{{ urlProcessor::makeUrl( $modx->getConfig('site_start') ) }}" title="{{ $modx->getConfig('site_start') }}" itemprop="url">
+				<a class="logo" href="@makeUrl( $modx->getConfig('site_start') )" title="{{ $modx->getConfig('site_name') }}" itemprop="url">
 					<img src="{{ $modx->getConfig('site_url') }}manager/media/style/default/images/misc/login-logo.png" itemprop="logo" alt="{{ $modx->getConfig('site_name') }}" />
 				</a>
 
@@ -20,16 +20,28 @@
 						</button>
 					</div>
 					<div id="navbar" class="navbar-collapse collapse">
-						
-						{!! $modx->runSnippet('DLMenu',[
-							'parents' => '0',
-							'maxDepth' => '2',
-							'outerClass' => 'nav navbar-nav',
-							'hereClass' => 'active',
-							'innerTpl' => '@CODE:<ul class="dropdown-menu">[+wrap+]</ul>',
-							'parentRowTpl' => '@CODE:<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" title="[+title+]">[+title+] <b class="caret"></b></a>[+wrap+]</li>',
-						]) !!}
-						
+
+						<ul class="nav navbar-nav">
+							@foreach($topmenu as $item)
+								@if( isset($item['children']) )
+									<li class="dropdown @if($item['id'] == $documentObject['id']) active @endif">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ $item['title'] }} <b class="caret"></b></a>
+										<ul class="dropdown-menu">
+											@foreach($item['children'] as $level2)
+												<li class="@if($level2['id'] == $documentObject['id']) active @endif">
+													<a href="{{ $level2['url'] }}">{{ $level2['title'] }}</a>
+												</li>
+											@endforeach
+										</ul>
+									</li>
+								@else
+									<li class="@if($item['id'] == $documentObject['id']) active @endif">
+										<a href="{{ $item['url'] }}">{{ $item['title'] }}</a>
+									</li>
+								@endif
+							@endforeach
+						</ul>
+
 					</div>
 				</nav>
 				<div class="affspacer"></div>
